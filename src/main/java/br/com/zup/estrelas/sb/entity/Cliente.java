55b9d39d52b6a3e3a431b455280d4cbaef4ac4fa
GrazielaRO <grazi375@hotmail.com>
2020-11-23 17:16:15 -0300

    criação entity cliente, usuário e enum tipoUsuario

--- src/main/java/br/com/zup/estrelas/sb/entity/Cliente.java
+++ src/main/java/br/com/zup/estrelas/sb/entity/Cliente.java
@@ -1,5 +1,31 @@
 package br.com.zup.estrelas.sb.entity;
 
-public class Cliente {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import br.com.zup.estrelas.sb.abstrata.Usuario;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Cliente extends Usuario {
+    
+    @Id
+    @Column(name = "id_cliente")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCliente;
+    
+    @Column(nullable = false)
+    private String cpf;
+    
+    @Column(nullable = false)
+    private String genero;
+    
+    @Column(name= "data_nascimento", nullable = false)
+    private String dataNascimento; 
 
 }
