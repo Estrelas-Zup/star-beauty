b20a404fa8cacfcfc331ede84b85432e720d7b3e
joaoalveszup <joao.alves@zup.com.br>
2020-11-23 10:13:26 -0300

    Commit inicial.

---
+++ src/main/java/br/com/zup/estrelas/sb/StarBeautyApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBeautyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarBeautyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/zup/estrelas/sb/StarBeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarBeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
