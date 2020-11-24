0a46b135b42e5237c7dcbf9c86c6f42d026e198a
unknown <eliaslima900@hotmail.com>
2020-11-24 15:40:47 -0300

    criação de entity agendamento e transação

--- src/main/java/br/com/zup/estrelas/sb/entity/Agendamento.java
+++ src/main/java/br/com/zup/estrelas/sb/entity/Agendamento.java
@@ -1,5 +1,49 @@
 package br.com.zup.estrelas.sb.entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import br.com.zup.estrelas.sb.enums.FormaPagamento;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Agendamento {
+
+    @Id
+    @Column(name = "id_agendamento")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idAgendamento;
+
+    private Funcionario funcionario; // chave estrangeira
+
+    private Cliente cliente; // chave estrangeira
+
+    private Servico servico; // chave estrangeiraS
+
+    @Column(name = "nome_cliente", nullable = false)
+    private String nomeCliente;
+
+    @Column(name = "nome_servico", nullable = false)
+    private String nomeServico;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(nullable = false)
+    private LocalDateTime hora;
+
+    @Column(name = "forma_pagamento", nullable = false)
+    private FormaPagamento formaPagamento;
+
+    private boolean realizado;
+
+    private boolean cancelado;
 
 }
---
+++ src/main/java/br/com/zup/estrelas/sb/entity/Transacao.java
@@ -0,0 +1,32 @@
+package br.com.zup.estrelas.sb.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import br.com.zup.estrelas.sb.enums.FormaPagamento;
+
+@Entity
+public class Transacao {
+
+    @Id
+    @Column(name = "id_transacao")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTransacao;
+
+    private Agendamento agendamento; // chave estrangeira
+
+    @Column(nullable = false)
+    private Double valor;
+
+    @Column(name = "forma_pagamento", nullable = false)
+    private FormaPagamento formaPagmento;
+
+    @Column(name = "nome_cliente", nullable = false)
+    private String nomeCliente;
+
+    @Column(name = "nome_funcionario", nullable = false)
+    private String nomeFuncionario;
+
+}
