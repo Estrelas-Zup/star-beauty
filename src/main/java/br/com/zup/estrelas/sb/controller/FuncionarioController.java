4f1b82177d3210af19caeac38500984875091895
DayanaSouzaZup <dayana.souza@zup.com.br>
2020-11-26 15:20:56 -0300

    Crud de funcionário finalizado

--- src/main/java/br/com/zup/estrelas/sb/controller/FuncionarioController.java
+++ src/main/java/br/com/zup/estrelas/sb/controller/FuncionarioController.java
@@ -1,5 +1,66 @@
 package br.com.zup.estrelas.sb.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.estrelas.sb.dto.FuncionarioDTO;
+import br.com.zup.estrelas.sb.dto.InativaFuncionarioDTO;
+import br.com.zup.estrelas.sb.dto.MensagemDTO;
+import br.com.zup.estrelas.sb.entity.Funcionario;
+import br.com.zup.estrelas.sb.service.FuncionarioService;
+
+@RestController
+@RequestMapping("/funcionarios")
 public class FuncionarioController {
+
+    @Autowired
+    FuncionarioService funcionarioService;
+
+    @PostMapping
+    public MensagemDTO adicionaFuncionario(@RequestBody FuncionarioDTO funcionario) {
+
+        return funcionarioService.adicionaFuncionario(funcionario);
+    }
+
+    @GetMapping(path = "/{idFuncionario}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Funcionario buscaFuncionario(@PathVariable Long idFuncionario) {
+
+        return funcionarioService.buscaFuncionario(idFuncionario);
+
+    }
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public List<Funcionario> listaFuncionarios() {
+
+        return funcionarioService.listaFuncionarios();
+    }
+
+    @DeleteMapping(path = "/{idFuncionario}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public MensagemDTO removeFuncionario(@PathVariable Long idFuncionario) {
+
+        return funcionarioService.removeFuncionario(idFuncionario);
+    }
+
+    @PutMapping(path = "/{idFuncionario}")
+    public MensagemDTO alteraFuncionario(@PathVariable Long idFuncionario,
+            @RequestBody FuncionarioDTO alteraFuncionarioDTO) {
+
+        return funcionarioService.alteraFuncionario(idFuncionario, alteraFuncionarioDTO);
+    }
+
+    @PutMapping(path = "/{idFuncionario}/inativa")
+    public MensagemDTO inativaFuncionario(@PathVariable Long idFuncionario,
+            @RequestBody InativaFuncionarioDTO inativaFuncionarioDTO) {
+
+        return funcionarioService.inativaFuncionario(idFuncionario, inativaFuncionarioDTO);
+    }
 
 }
