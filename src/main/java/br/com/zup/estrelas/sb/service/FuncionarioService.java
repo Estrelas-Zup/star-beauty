4dbdd81081515ba70dd7149362974718d157a120
DayanaSouzaZup <dayana.souza@zup.com.br>
2020-11-25 23:51:23 -0300

    Crud de Funcionário iniciado

--- src/main/java/br/com/zup/estrelas/sb/service/FuncionarioService.java
+++ src/main/java/br/com/zup/estrelas/sb/service/FuncionarioService.java
@@ -1,5 +1,21 @@
 package br.com.zup.estrelas.sb.service;
 
+import java.util.List;
+import br.com.zup.estrelas.sb.dto.AlteraFuncionarioDTO;
+import br.com.zup.estrelas.sb.dto.MensagemDTO;
+import br.com.zup.estrelas.sb.entity.Funcionario;
+
 public interface FuncionarioService {
+    
+    public MensagemDTO adicionaFuncionario(Funcionario funcionario);
+    
+    public Funcionario buscaFuncionario(Long idFuncionario);
+    
+    public List<Funcionario> listaFuncionarios();
+    
+    public MensagemDTO removeFuncionario(Long idFuncionario);
+    
+    public MensagemDTO alteraFuncionario(Long idFuncionario,AlteraFuncionarioDTO alteraFuncionarioDTO);
+    
 
 }
