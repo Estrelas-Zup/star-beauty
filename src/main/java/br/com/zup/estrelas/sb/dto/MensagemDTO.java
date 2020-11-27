5c19cd56702bc69fdc45347d48fe47d918c6f677
unknown <eliaslima900@hotmail.com>
2020-11-27 14:40:39 -0300

    Criação CRUD agendamento

--- src/main/java/br/com/zup/estrelas/sb/dto/MensagemDTO.java
+++ src/main/java/br/com/zup/estrelas/sb/dto/MensagemDTO.java
@@ -77,4 +77,43 @@ public class MensagemDTO {
         return true;
     }
 
+    private String mensagem;
+
+    public MensagemDTO(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((mensagem == null) ? 0 : mensagem.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MensagemDTO other = (MensagemDTO) obj;
+        if (mensagem == null) {
+            if (other.mensagem != null)
+                return false;
+        } else if (!mensagem.equals(other.mensagem))
+            return false;
+        return true;
+    }
+
 }
