201632fa98d8cc6b08db1a711384e6d75d199aaf
unknown <eliaslima900@hotmail.com>
2020-11-27 14:40:39 -0300

    Criação CRUD agendamento

--- src/main/java/br/com/zup/estrelas/sb/service/AgendamentoService.java
+++ src/main/java/br/com/zup/estrelas/sb/service/AgendamentoService.java
@@ -1,5 +1,25 @@
 package br.com.zup.estrelas.sb.service;
 
+import java.util.List;
+import br.com.zup.estrelas.sb.dto.AgendamentoDTO;
+import br.com.zup.estrelas.sb.dto.CancelaAgendamentoDTO;
+import br.com.zup.estrelas.sb.dto.FinalizaAgendamentoDTO;
+import br.com.zup.estrelas.sb.dto.MensagemDTO;
+import br.com.zup.estrelas.sb.entity.Agendamento;
+
 public interface AgendamentoService {
 
+    public Agendamento buscaAgendamento(Long idAgendamento);
+
+    public List<Agendamento> listaAgendamento();
+
+    public MensagemDTO criaAgendamento(AgendamentoDTO agendamentoDTO);
+
+    public MensagemDTO alteraAgendamento(Long idAgendamento, AgendamentoDTO agendamentoDTO);
+
+    public MensagemDTO finalizaAgendamento(Long idAgendamento,
+            FinalizaAgendamentoDTO finalizaAgendamentoDTO);
+
+    public MensagemDTO cancelaAgendamento(Long idAgendamento,
+            CancelaAgendamentoDTO cancelaAgendamentoDTO);
 }
