12be6beb7d1b08eefa549fb038f9e891019b1638
GrazielaRO <grazi375@hotmail.com>
2020-12-01 10:33:56 -0300

    atualização ServicoController

--- src/main/java/br/com/zup/estrelas/sb/controller/ServicoController.java
+++ src/main/java/br/com/zup/estrelas/sb/controller/ServicoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
