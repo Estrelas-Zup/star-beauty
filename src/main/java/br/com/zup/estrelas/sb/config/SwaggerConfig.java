d1e15f804185b23a607abc4e0122344332ea5da3
DayanaSouzaZup <dayana.souza@zup.com.br>
2020-12-03 19:00:39 -0300

    Método metaData adicionado

--- src/main/java/br/com/zup/estrelas/sb/config/SwaggerConfig.java
+++ src/main/java/br/com/zup/estrelas/sb/config/SwaggerConfig.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -22,7 +24,7 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any()).build();
+                .paths(PathSelectors.any()).build().apiInfo(metaData());
     }
 
     private List<ResponseMessage> responseMessageForGET() {
@@ -42,5 +44,12 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any()).build().useDefaultResponseMessages(false)
                 .globalResponseMessage(RequestMethod.GET, responseMessageForGET());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("\"Spring Boot REST API\"").version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"").build();
     }
 }
