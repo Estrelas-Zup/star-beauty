5d12c1a387850b71f01bfdea54615166a262d28b
unknown <eliaslima900@hotmail.com>
2020-12-07 14:25:21 -0300

    Refaimplementação do handler de erros para validações de entrada

--- src/main/java/br/com/zup/estrelas/sb/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/zup/estrelas/sb/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,19 @@
 package br.com.zup.estrelas.sb.exceptions;
 
+import static java.util.Objects.nonNull;
+
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import br.com.zup.estrelas.sb.dto.ErrorDTO;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +26,28 @@ public class GlobalExceptionHandler {
         error.setStatus((HttpStatus.UNPROCESSABLE_ENTITY.value()));
         return new ResponseEntity<>(error, HttpStatus.UNPROCESSABLE_ENTITY);
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+
+    public @ResponseBody List<ErrorDTO> handleValidationError(MethodArgumentNotValidException e) {
+
+        List<ErrorDTO> errosDeValidacao = new ArrayList<>();
+
+        for (ObjectError erro : e.getBindingResult().getAllErrors()) {
+
+            if (nonNull(erro.getCodes())) {
+
+                StringBuilder mensagemASerExibida = new StringBuilder();
+                mensagemASerExibida.append(erro.getDefaultMessage());
+
+                errosDeValidacao.add(new ErrorDTO(mensagemASerExibida.toString()));
+            }
+        }
+
+        return errosDeValidacao;
+    }
+
+    // Como cobrir as outras excepitions lançadas pelo sistema?
 
 }
