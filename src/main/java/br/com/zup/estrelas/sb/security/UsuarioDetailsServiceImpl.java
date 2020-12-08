ca96ad7abf51bdbd88ed08f76109524d82961fa5
unknown <eliaslima900@hotmail.com>
2020-12-08 18:46:05 -0300

    ALTERAÇÕES GERAIS

--- src/main/java/br/com/zup/estrelas/sb/security/UsuarioDetailsServiceImpl.java
+++ src/main/java/br/com/zup/estrelas/sb/security/UsuarioDetailsServiceImpl.java
@@ -11,13 +11,15 @@ import br.com.zup.estrelas.sb.repository.UsuarioRepository;
 @Service
 public class UsuarioDetailsServiceImpl implements UserDetailsService {
 
+    private static final String USUÁRIO_NÃO_ENCONTRADO = "USUÁRIO NÃO ENCONTRADO!";
+    
     @Autowired
     UsuarioRepository usuarioRepository;
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         Usuario usuario = usuarioRepository.findByLogin(login)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));;
+                .orElseThrow(() -> new UsernameNotFoundException(USUÁRIO_NÃO_ENCONTRADO));;
 
         return new UsuarioDetails(usuario);
     }
