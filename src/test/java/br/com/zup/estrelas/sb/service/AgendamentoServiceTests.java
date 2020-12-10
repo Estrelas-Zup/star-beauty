8e98e870368127afc9658ee2ed5635fa1f05784d
DayanaSouzaZup <dayana.souza@zup.com.br>
2020-12-09 22:48:56 -0300

    Iniciados testes unitários Classe Funcionários

---
+++ src/test/java/br/com/zup/estrelas/sb/service/AgendamentoServiceTests.java
@@ -0,0 +1,27 @@
+package br.com.zup.estrelas.sb.service;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import br.com.zup.estrelas.sb.entity.Agendamento;
+import br.com.zup.estrelas.sb.repository.AgendamentoRepository;
+
+public class AgendamentoServiceTests {
+    
+    @Mock
+    AgendamentoRepository agendamentoRepository;
+    
+    @InjectMocks
+    AgendamentoService agegendamentoService;
+    
+    @Test
+    public void naoDeveRemoverUmAgendamento() {
+        
+        Agendamento agendamento = new Agendamento();
+        
+        Mockito.when(agendamentoRepository.findById(1L)).thenReturn(false);
+        
+    }
+
+}
