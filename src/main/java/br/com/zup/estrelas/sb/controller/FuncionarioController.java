3dbaad8f289d4d83f7032d20f0e48786a55d50d5
GrazielaRO <grazi375@hotmail.com>
2020-12-13 21:01:52 -0300

    cors implementation

--- src/main/java/br/com/zup/estrelas/sb/controller/FuncionarioController.java
+++ src/main/java/br/com/zup/estrelas/sb/controller/FuncionarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import br.com.zup.estrelas.sb.service.FuncionarioService;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@CrossOrigin 
 @PreAuthorize("hasAuthority('salao')")
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
--- src/main/java/br/com/zup/estrelas/sb/controller/ProfissionalAutonomoController.java
+++ src/main/java/br/com/zup/estrelas/sb/controller/ProfissionalAutonomoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import br.com.zup.estrelas.sb.service.ProfissionalAutonomoService;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@CrossOrigin 
 @RequestMapping("/autonomos")
 public class ProfissionalAutonomoController {
 
