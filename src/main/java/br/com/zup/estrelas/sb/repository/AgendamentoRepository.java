c76ae04d005436b5e965019b2d359ecd68400893
joaoalveszup <joao.alves@zup.com.br>
2020-12-15 04:00:06 -0300

    Adiciona query que testa horários.

--- src/main/java/br/com/zup/estrelas/sb/repository/AgendamentoRepository.java
+++ src/main/java/br/com/zup/estrelas/sb/repository/AgendamentoRepository.java
@@ -1,6 +1,7 @@
 package br.com.zup.estrelas.sb.repository;
 
 import java.time.LocalDateTime;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import br.com.zup.estrelas.sb.entity.Agendamento;
@@ -13,7 +14,11 @@ public interface AgendamentoRepository extends CrudRepository<Agendamento, Long>
 
     boolean existsByAutonomoIdUsuarioAndDataHora(Long idUsuario, LocalDateTime dataHora);
 
-    boolean existsByFuncionarioIdFuncionarioAndDataHoraBetween(Long idFuncionario,
-            LocalDateTime dataHoraAgendamento, LocalDateTime dataHoraFimAgendamento);
-
+    @Query("select case when (count(a) > 0)  then true else false end from Agendamento a where a.funcionario.idFuncionario = :idFuncionario and (a.dataHora between :dataHoraInicio and :dataHoraFim or a.dataHoraFim between :dataHoraInicio and :dataHoraFim or (a.dataHora < :dataHoraInicio and a.dataHoraFim > :dataHoraInicio))")
+    boolean existsByFuncionarioAgenda(
+            Long idFuncionario, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim);
+    
+    @Query("select case when (count(a) > 0)  then true else false end from Agendamento a where a.autonomo.idUsuario = :idAutonomo and (a.dataHora between :dataHoraInicio and :dataHoraFim or a.dataHoraFim between :dataHoraInicio and :dataHoraFim or (a.dataHora < :dataHoraInicio and a.dataHoraFim > :dataHoraInicio))")
+    boolean existsByAutonomoAgenda(
+            Long idAutonomo, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim);
 }
