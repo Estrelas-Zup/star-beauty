9242796f447c510fba58c8fdf1bf58fed18f7183
joaoalveszup <joao.alves@zup.com.br>
2020-12-16 16:35:22 -0300

    Adiciona filtros de CORS

--- src/main/java/br/com/zup/estrelas/sb/config/WebSecurityConfig.java
+++ src/main/java/br/com/zup/estrelas/sb/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.zup.estrelas.sb.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import br.com.zup.estrelas.sb.security.JwtAuthenticationEntryPoint;
 import br.com.zup.estrelas.sb.security.JwtRequestFilter;
 import br.com.zup.estrelas.sb.security.UsuarioDetailsServiceImpl;
@@ -61,6 +65,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+    
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 
 }
