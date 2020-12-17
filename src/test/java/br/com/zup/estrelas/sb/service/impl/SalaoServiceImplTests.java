ff2e2bfd3d4879b528cc405aa7612c55f5194790
GrazielaRO <grazi375@hotmail.com>
2020-12-16 21:11:28 -0300

    salao tests

--- src/test/java/br/com/zup/estrelas/sb/service/impl/SalaoServiceImplTests.java
+++ src/test/java/br/com/zup/estrelas/sb/service/impl/SalaoServiceImplTests.java
@@ -1,6 +1,8 @@
 package br.com.zup.estrelas.sb.service.impl;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,27 +12,45 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import br.com.zup.estrelas.sb.dto.FormaPagamentoDTO;
+import br.com.zup.estrelas.sb.dto.InativaSalaoDTO;
 import br.com.zup.estrelas.sb.dto.SalaoDTO;
+import br.com.zup.estrelas.sb.entity.FormaPagamento;
 import br.com.zup.estrelas.sb.entity.Salao;
+import br.com.zup.estrelas.sb.enums.TipoPagamento;
+import br.com.zup.estrelas.sb.enums.TipoUsuario;
 import br.com.zup.estrelas.sb.exceptions.RegrasDeNegocioException;
+import br.com.zup.estrelas.sb.repository.FormaPagamentoRepository;
 import br.com.zup.estrelas.sb.repository.SalaoRepository;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SalaoServiceImplTests {
-    
+
     private static final String SALAO_JA_POSSUI_CADASTRO = "JÁ EXISTE UM CADASTRO PARA ESSE SALÃO.";
+    private static final String SALAO_NAO_ENCONTRADO =
+            "O SALÃO NÃO FOI ENCONTRADO! VERIFIQUE AS INFORMAÇÕES INSERIDAS.";
+    private static final String CNPJ_ALTERADO_JA_EXISTE_NO_BANCO_DE_DADOS =
+            "CNPJ ALTERADO JÁ EXISTE NO BANCO DE DADOS!";
+    private static final String SALAO_NAO_ENCONTRADO_PARA_SER_INATIVADO =
+            "O SALÃO NÃO FOI ENCONTRADO PARA SER INATIVADO!";
 
     @Mock
     SalaoRepository salaoRepository;
+    
+    @Mock
+    FormaPagamentoRepository formaPagamentoRepository;
 
     @Mock
     PasswordEncoder passwordEncoder;
+    
+    @Mock
+    List<FormaPagamento> formasPagamento; 
 
     @InjectMocks
     SalaoServiceImpl salaoServiceImpl;
 
     @Test
-    public void deveAdicionarUmSalao() throws RegrasDeNegocioException {
+    public void deveAdicionarUmSalaoComSucesso() throws RegrasDeNegocioException {
 
         SalaoDTO salaoDTO = this.SalaoDtoFactory();
 
@@ -47,9 +67,9 @@ public class SalaoServiceImplTests {
 
         Assert.assertEquals(salaoEsperado, salaoRetornado);
     }
-    
+
     @Test
-    public void naoDeveAdicionarUmSalao(){
+    public void naoDeveAdicionarUmSalao() {
 
         SalaoDTO salaoDTO = this.SalaoDtoFactory();
 
@@ -57,13 +77,169 @@ public class SalaoServiceImplTests {
 
         String erroEsperado = SALAO_JA_POSSUI_CADASTRO;
         String erroRetornado = null;
-        
+
         try {
             salaoServiceImpl.adicionaSalao(salaoDTO);
         } catch (RegrasDeNegocioException e) {
             erroRetornado = e.getMensagemErro();
         }
 
+        Assert.assertEquals(erroEsperado, erroRetornado);
+    }
+
+    @Test
+    public void deveAlterarSalaoComSucesso() throws RegrasDeNegocioException {
+
+        SalaoDTO salaoDTO = this.SalaoDtoFactory();
+        Optional<Salao> salao = Optional.of(this.salaoFactory());
+
+        Mockito.when(salaoRepository.existsById(1L)).thenReturn(true);
+
+        Mockito.when(salaoRepository.findById(1L)).thenReturn(salao);
+
+        Salao salaoRetornado = salaoServiceImpl.alteraSalao(1L, salaoDTO);
+        Salao salaoEsperado = new Salao();
+
+        BeanUtils.copyProperties(salao.get(), salaoEsperado);
+
+        Assert.assertEquals(salaoEsperado, salaoRetornado);
+    }
+
+    @Test
+    public void NaoDeveAlterarSalaoCasoNaoExista() {
+
+        SalaoDTO salaoDTO = this.SalaoDtoFactory();
+
+        String erroEsperado = SALAO_NAO_ENCONTRADO;
+        String erroRetornado = null;
+
+
+        try {
+            salaoServiceImpl.alteraSalao(1L, salaoDTO);
+        } catch (RegrasDeNegocioException e) {
+            erroRetornado = e.getMensagemErro();
+        }
+
+        Assert.assertEquals(erroEsperado, erroRetornado);
+    }
+
+    @Test
+    public void NaoDeveAlterarSalaoCasoCnpjJaExista() {
+
+        SalaoDTO salaoDTO = this.SalaoDtoFactory();
+
+        Optional<Salao> salao = Optional.of(this.salaoFactory());
+
+        Mockito.when(salaoRepository.existsById(1L)).thenReturn(true);
+
+        Mockito.when(salaoRepository.findById(1L)).thenReturn(salao);
+
+        Assert.assertFalse(salaoDTO.getCnpj().equals(salao.get().getCnpj()));
+
+        Mockito.when(salaoRepository.existsByCnpj(salaoDTO.getCnpj())).thenReturn(true);
+
+        String erroEsperado = CNPJ_ALTERADO_JA_EXISTE_NO_BANCO_DE_DADOS;
+        String erroRetornado = null;
+
+        try {
+            salaoServiceImpl.alteraSalao(1L, salaoDTO);
+        } catch (RegrasDeNegocioException e) {
+            erroRetornado = e.getMensagemErro();
+        }
+
+        Assert.assertEquals(erroEsperado, erroRetornado);
+
+    }
+
+    @Test
+    public void deveInativarSalaoComSucesso() throws RegrasDeNegocioException {
+
+        InativaSalaoDTO inativaSalaoDTO = new InativaSalaoDTO();
+        inativaSalaoDTO.setAtivo(false);
+        
+        Optional<Salao> salao = Optional.of(this.salaoFactory());
+        
+        Mockito.when(salaoRepository.existsById(1L)).thenReturn(true);
+        
+        Mockito.when(salaoRepository.findById(1L)).thenReturn(salao);
+        
+        Salao salaoRetornado = salaoServiceImpl.inativaSalao(1L, inativaSalaoDTO);
+        Salao salaoEsperado = new Salao();
+        
+        BeanUtils.copyProperties(salao.get(), salaoEsperado);
+        salaoEsperado.setAtivo(inativaSalaoDTO.isAtivo());
+        
+        Assert.assertEquals(salaoEsperado, salaoRetornado);
+    }
+    
+    @Test
+    public void NaoDeveInativarSalao(){
+        
+        InativaSalaoDTO inativaSalaoDTO = new InativaSalaoDTO();
+        inativaSalaoDTO.setAtivo(false);
+        
+        String erroEsperado = SALAO_NAO_ENCONTRADO_PARA_SER_INATIVADO;
+        String erroRetornado = null;
+        
+        try {
+            salaoServiceImpl.inativaSalao(1L, inativaSalaoDTO);
+        } catch (RegrasDeNegocioException e) {
+            erroRetornado = e.getMensagemErro();
+        }
+        
+        Assert.assertEquals(erroEsperado, erroRetornado);
+    }
+    
+    @Test
+    public void deveAdicionarFormaDePagamentoComSucesso() throws RegrasDeNegocioException{
+        
+        FormaPagamentoDTO formaPagamentoDTO = new FormaPagamentoDTO();
+        formaPagamentoDTO.setTipoPagamento(TipoPagamento.DINHEIRO);
+        
+        Optional<FormaPagamento> formaPagamento = Optional.of(this.formaPagamentoFactory());
+        
+        Optional<Salao> salao = Optional.of(this.salaoFactory());
+        
+        Mockito.when(salaoRepository.existsById(1L)).thenReturn(true);
+        
+        Mockito.when(formaPagamentoRepository.existsById(1L)).thenReturn(true);
+        
+        Mockito.when(salaoRepository.findById(1L)).thenReturn(salao);
+
+        Mockito.when(formaPagamentoRepository.findById(1L)).thenReturn(formaPagamento);
+        
+        Mockito.when(formasPagamento.add(formaPagamento.get())).thenReturn(true);
+
+        Mockito.when(formasPagamento.contains(formaPagamento.get())).thenReturn(false);
+        
+        salao.get().setFormasPagamento(formasPagamento);
+        
+        Salao salaoRetornado = salaoServiceImpl.adicionaFormaPagamento(1L, formaPagamentoDTO);
+        Salao salaoEsperado = new Salao();
+        
+        BeanUtils.copyProperties(salao.get(), salaoEsperado);
+        
+        Assert.assertEquals(salaoEsperado, salaoRetornado);
+        
+    }
+    
+    @Test
+    public void naoDeveAdicionarFormaDePagamentoComSucesso() {
+        
+        FormaPagamentoDTO formaPagamentoDTO = new FormaPagamentoDTO();
+        formaPagamentoDTO.setTipoPagamento(TipoPagamento.DINHEIRO);
+        
+        Mockito.when(salaoRepository.existsById(1L)).thenReturn(false);
+        
+        String erroEsperado = SALAO_NAO_ENCONTRADO;
+        String erroRetornado = null;
+        
+        try {
+            salaoServiceImpl.adicionaFormaPagamento(1L, formaPagamentoDTO);
+        } catch (RegrasDeNegocioException e) {
+            erroRetornado = e.getMensagemErro();
+        }
+        
         Assert.assertEquals(erroEsperado, erroRetornado);
     }
 
@@ -86,5 +262,41 @@ public class SalaoServiceImplTests {
 
         return salaoDTO;
     }
-  
+
+    private Salao salaoFactory() {
+
+        Salao salao = new Salao();
+
+        salao.setIdUsuario(1L);
+        salao.setLogin("graziela@mail.com");
+        salao.setSenha("14GdfG98");
+        salao.setNome("Grazi Beauty");
+        salao.setEndereco("Rua do Protetor, 456");
+        salao.setCep("38407509");
+        salao.setEstado("Minas Gerais");
+        salao.setCidade("Uberlândia");
+        salao.setBairro("Loteamento Integração");
+        salao.setTelefone("34994586521");
+        salao.setEmail("graziela@mail.com");
+        salao.setTipoUsuario(TipoUsuario.SALAO);
+        salao.setCnpj("27810546000165");
+        salao.setNomeFantasia("Beauty Factory");
+        salao.setFormasPagamento(Collections.emptyList());
+        salao.setFuncionarios(Collections.emptyList());
+        salao.isAtivo();
+
+        return salao;
+
+    }
+    
+    private FormaPagamento formaPagamentoFactory() {
+        
+        FormaPagamento formaPagamento = new FormaPagamento();
+        
+        formaPagamento.setIdFormaPagamento(1L);
+        formaPagamento.setTipoPagamento(TipoPagamento.DINHEIRO);
+        
+        return formaPagamento;
+    }
+
 }
